e66d91e22ac2da7043ffec5f904bf4754bbcd0ee
balazsmanus <contact@balazsmanus.hu>
2024-05-01 20:48:33 +0200

    finish some buttons

--- js/mapinit.js
+++ js/mapinit.js
@@ -1,6 +1,6 @@
 var map = L.map('map', { attributionControl: false, minZoom: 6, zoomControl: false }).setView([47.184, 19.509], 8);
 
-L.control.zoom({ position: 'topleft', zoomInText: '<img src="/icons/plus.svg">', zoomOutText: '<img src="/icons/minus.svg">' }).addTo(map);
+L.control.zoom({ position: 'topleft', zoomInText: '<div class="navy-button"><img src="/icons/plus.svg"></div>', zoomOutText: '<div class="navy-button"><img src="/icons/minus.svg"></div>' }).addTo(map);
 L.control.locate({ setView: false }).addTo(map);
 
 L.control.attribution({ position: 'bottomright', prefix: false }).addAttribution(`
