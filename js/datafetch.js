f0930619958aeece41fa4134521b96cf8e219c82
balazsmanus <contact@balazsmanus.hu>
2024-05-04 08:52:49 +0200

    add new not completed sticker thing

--- js/datafetch.js
+++ js/datafetch.js
@@ -4,36 +4,45 @@ fetch('/data/data.json')
         var entries = 0;
         data.forEach(item => {
             entries++;
-            var markerIcon = (item.type === "complete" || item.type === "communityfound") ? stickerIcon : partialIcon;
-            var MarkerGroup = item.type === "complete" ? stickerMarkers : partialStickerMarkers;
-            var extrainfo = (item.type === "complete" || item.type === "communityfound") ? "" : '<a class="missinginfo" href="https://forms.gle/gCpWT2j4qARYVCz58">Az információ hiányos, ha megtalálod, jelezd!</a>';
-            var finderinfo = item.type === "communityfound" ? `<p class="communityfound">Kiegészítette, ${item.finder}.</p>` : "";
+            const markerIcon = (item.type === "complete" || item.type === "communityfound") ? stickerIcon : partialIcon;
+            const MarkerGroup = item.type === "complete" ? stickerMarkers : partialStickerMarkers;
+            const finderinfo = item.type === "communityfound" ? `<p class="communityfound">Készítette, ${item.finder}.</p>` : "";
+            let popupData = (item.type === "complete" || item.type === "communityfound") ? `
+            <div>
+                <h3>${item.popup.title}</h3>
+                <p class="popupdesc">${item.popup.address}</p>
+            </div>
+            <div class="image-container">
+                <div id="${item.popup.image}" class="spinner center">
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                    <div class="spinner-blade"></div>
+                </div>
+                <img class="pending popupimg" src="${item.popup.image}" width="100%" onload="document.getElementById('${item.popup.image}').remove();this.classList.remove('pending');">
+            </div>
+            ${finderinfo}` : `
+            <div>
+                <h3>${item.popup.title}</h3>
+                <p class="popupdesc">${item.popup.address}</p>
+                <div class="missingdata">
+                    <img src="/icons/sad.svg">
+                    <p class="missinginfo">Ez a matrica nincs dokumentálva,<br>
+                    ha megtaláltad, <a class="missinginfo" href="https://forms.gle/gCpWT2j4qARYVCz58">jelezd</a>!</p>
+                </div>
+            </div>
+            `;
+
             var marker = L.marker([item.z, item.x], { icon: markerIcon })
-                .bindPopup(`
-                <div>
-                    <h3>${item.popup.title}</h3>
-                    <p class="popupdesc">${item.popup.address}</p>
-                </div>
-                <div class="image-container">
-                    <div id="${item.popup.image}" class="spinner center">
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                        <div class="spinner-blade"></div>
-                    </div>
-                    <img class="pending popupimg" src="${item.popup.image}" width="100%" onload="document.getElementById('${item.popup.image}').remove();this.classList.remove('pending');">
-                </div>
-                ${extrainfo}
-                ${finderinfo}
-            `);
+                .bindPopup(popupData);
             MarkerGroup.addLayer(marker);
         });
         console.info(`Found ${entries} entries.`)
